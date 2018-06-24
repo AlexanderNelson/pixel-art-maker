e747eafe0dbe9789e6a4142d26a6dc2ba0e24804
Alexander Nelson <amvetserv@gmail.com>
2018-06-23 20:20:11 -0400

    add function to change square color to selected color when clicked

--- designs.js
+++ designs.js
@@ -1,43 +1,35 @@
-// Select color input
-//console.log($("#colorPicker").val());//test
-// Select size input
-//console.log($("#inputHeight").val());//test
-//console.log($("#inputWidth").val());//test
+//  Select color input
+// console.log($("#colorPicker").val());
+//  Select size input
+// console.log($("#inputHeight").val());
+// console.log($("#inputWidth").val());
 
 $("input[type='submit']").on("click", function(evt) {
 	event.preventDefault();
-	//console.log("watchuwant"); //test
-
+	//console.log("watchuwant"); 
 	$("#pixelCanvas").empty();
-
 	const height = $("#inputHeight").val();
 	const width = $("#inputWidth").val();
-	//console.log(height, width + " as arguments"); //test
-
 	makeGrid(height, width);
 });
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid(height, width) {
-
-// Your code goes here!
-	//console.log("tapptapp"); //test
-	//console.log(height, width + ' in makeGrid function'); //test
-
 	//for grid height input
 	for (let h = 0; h < height; h++) {
-	
-	//create number of blocks in column
-	$("#pixelCanvas").append($("<tr></tr>"));
-	//console.log("input----height" + " " + h);//test
-
-	//for grid width input
-	for (let w = 0; w < width; w++) {
-
-	// create table of width
-	$("tr").last().append($("<td></td>"));
-	console.log("input width" + " " + w);//test
+		//create number of blocks in column
+		$("#pixelCanvas").append($("<tr></tr>"));
+		//console.log("input----height" + " " + h);
+		//for grid width input
+		for (let w = 0; w < width; w++) {
+			// create table of width
+			$("tr").last().append($("<td></td>"));
+			//console.log("input width" + " " + w);
+		}
 	}
-}
+
+	//change background color when clicked
+	$("#pixelCanvas").on("click", "td", function() {
+		$(this).css("background-color", $("#colorPicker").val());
+	});
 }
